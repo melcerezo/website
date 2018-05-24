4fefd80797d877b3d0d6efb47e39b0b265437293
Melchor Cerezo <melchorcerezo@Melchors-MBP.attlocal.net>
2018-05-23 22:45:32 -0700

    included css framework buma, added about section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+// wait for page to load before registering events
 window.addEventListener('load',registerEvents,false);
 
+// register event listeners
 function registerEvents(e) {
+
+  // register when mouse is over nav links to change background color
   var links = document.getElementsByClassName("link");
   for(var i = 0; i <links.length; i++){
     links[i].addEventListener('mouseover',function(){
