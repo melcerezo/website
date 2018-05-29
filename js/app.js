540a2c89bdb0cd22d4fe28886565acfbaee915fa
Melchor Cerezo <melchorcerezo@Melchors-MBP.attlocal.net>
2018-05-28 18:06:39 -0700

    imported bulma directly into project, converted main.css to main.scss

--- js/app.js
+++ js/app.js
@@ -5,13 +5,26 @@ window.addEventListener('load',registerEvents,false);
 function registerEvents(e) {
 
   // register when mouse is over nav links to change background color
-  var links = document.getElementsByClassName("link");
+  var links = document.getElementsByClassName("anc-link");
   for(var i = 0; i <links.length; i++){
-    links[i].addEventListener('mouseover',function(){
-      this.style.backgroundColor = "#dadada";
-    }, false);
-    links[i].addEventListener('mouseout',function() {
-      this.style.backgroundColor = "#FFFFFF";
+    links[i].addEventListener('click',function(){
+      var thisTo = this.getAttribute('data-anchorId');
+      scrollTo(document.documentElement, document.getElementById(thisTo).offsetTop, 600)
     }, false);
   }
+}
+
+
+function scrollTo(element, to, duration){
+  if(duration <= 0){
+    return;
+  }
+  var difference = to - element.scrollTop;
+  var perTick = difference / duration * 10;
+
+  setTimeout(function() {
+    element.scrollTop = element.scrollTop + perTick;
+    if(element.scrollTop === to) return;
+    scrollTo(element, to, duration - 10);
+  }, 10);
 }
