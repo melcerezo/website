d1c25cd3439a88ced1528badb5cbe53b962f5723
Melchor Cerezo <melchorcerezo@Melchors-MBP.attlocal.net>
2018-05-29 13:50:54 -0700

    simplified code to have only one scroll function

--- js/app.js
+++ js/app.js
@@ -10,40 +10,48 @@ function registerEvents(e) {
     document.querySelector('.navbar-menu').classList.toggle('is-active');
   }, false);
 
-  // register when clicked
-
+  // register click event with each link in navbar
   var links = document.getElementsByClassName("anc-link");
-  window.addEventListener('resize', function() {
-    console.log(window.innerWidth)
-    if(window.innerWidth >= 800){
-      for(var i = 0; i <links.length; i++){
-        links[i].addEventListener('click',function(){
-          var thisTo = this.getAttribute('data-anchorId');
-          scrollTo(document.documentElement, document.getElementById(thisTo).offsetTop, 600)
-        }, false);
-      }
-    }else{
-      for(var i = 0; i <links.length; i++){
-        links[i].addEventListener('click',function(){
-          var thisTo = this.getAttribute('data-anchorId');
-          document.getElementById(thisTo).scrollIntoView('smooth');
-        }, false);
-      }
-    }
-  }, false);
+  for(var i = 0; i <links.length; i++){
+    links[i].addEventListener('click',function(){
+      var thisTo = this.getAttribute('data-anchorId');
+      document.getElementById(thisTo).scrollIntoView('smooth');
+    }, false);
+
+
 }
 
+// function windowSize(windowWidth, links){
+//   console.log(windowWidth);
+//   if(windowWidth >= 800){
+//     for(var i = 0; i <links.length; i++){
+//       links[i].removeEventListener
+//       links[i].addEventListener('click',function(){
+//         var thisTo = this.getAttribute('data-anchorId');
+//         scrollTo(document.documentElement, document.getElementById(thisTo).offsetTop, 600)
+//       }, false);
+//     }
+//   }else{
+//     for(var i = 0; i <links.length; i++){
+//       links[i].addEventListener('click',function(){
+//         var thisTo = this.getAttribute('data-anchorId');
+//         document.getElementById(thisTo).scrollIntoView('smooth');
+//       }, false);
+//     }
+//   }
+// }
 
-function scrollTo(element, to, duration){
-  if(duration <= 0){
-    return;
-  }
-  var difference = to - element.scrollTop;
-  var perTick = difference / duration * 10;
 
-  setTimeout(function() {
-    element.scrollTop = element.scrollTop + perTick;
-    if(element.scrollTop === to) return;
-    scrollTo(element, to, duration - 10);
-  }, 10);
-}
+// function scrollTo(element, to, duration){
+//   if(duration <= 0){
+//     return;
+//   }
+//   var difference = to - element.scrollTop;
+//   var perTick = difference / duration * 10;
+//
+//   setTimeout(function() {
+//     element.scrollTop = element.scrollTop + perTick;
+//     if(element.scrollTop === to) return;
+//     scrollTo(element, to, duration - 10);
+//   }, 10);
+// }
